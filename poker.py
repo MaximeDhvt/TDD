7f9dad5877bfa1fd3cc80d2d348f7bc022413ed0
DHIVERT <maximedhivert@hotmail.fr>
2025-03-04 14:59:40 +0100

    Ajout de la table de correspondance des rangs et amélioration de la classe Card. 

--- poker.py
+++ poker.py
@@ -1,13 +1,16 @@
-# poker.py
+import collections
+
+# Table de correspondance des rangs
+RANKS_ORDER = {
+    "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+    "7": 7, "8": 8, "9": 9, "10": 10,
+    "Valet": 11, "Dame": 12, "Roi": 13, "As": 14
+}
 
 class Card:
     def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
+        self.suit = suit      # "Coeur", "Carreau", "Trèfle", "Pique"
+        self.rank = rank      # "2", "3", …, "10", "Valet", "Dame", "Roi", "As"
 
     def __repr__(self):
         return f"{self.rank} de {self.suit}"
-
-def evaluate_hand(hand):
-    # Implémentation minimale pour toujours retourner "Carte Haute"
-    return {"rank_value": 1, "rank_name": "Carte Haute", "tiebreaker": []}
